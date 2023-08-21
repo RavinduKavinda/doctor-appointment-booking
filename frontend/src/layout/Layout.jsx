3b4d9b033c35874a005e5788110f9f0e714b6bac
RavinduKavinda <dlravindukavinda@gmail.com>
2023-08-21 17:24:37 +0530

    import header footer routers

--- frontend/src/layout/Layout.jsx
+++ frontend/src/layout/Layout.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import Header from '../components/Header/Header';
+import Footer from '../components/Footer/Footer';
+import Routers from '../routes/routes/Routers';
 
 function Layout() {
-  return (
-    <div>
-      Layout
-    </div>
+  return (<>
+    <Header/>
+    <main>
+      <Routers/>
+    </main>
+    <Footer/>
+    </>
   );
 }
 
