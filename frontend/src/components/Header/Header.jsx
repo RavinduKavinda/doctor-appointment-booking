6520cfd5105ecbc0f5fde2c1c3a8fbe9fada47e5
RavinduKavinda <dlravindukavinda@gmail.com>
2023-08-22 16:52:52 +0530

    logo added

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React from 'react';
+import logo from '../../assets/images/logo.png';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className="header flex items-center">
+      <div className="container">
+        <div className="flex items-center justify-between">
+          {/* Logo */}
+          <div>
+              <img src={logo} alt="logo" className="max-h-12 mt-2"/>
+          </div>   
+        </div>
+      </div>
+    </header>  
   )
 }
 
