6f0f3f54314c9426b123daef468beaee90287bf5
RavinduKavinda <dlravindukavinda@gmail.com>
2023-08-23 18:15:07 +0530

    margin remove

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -30,16 +30,16 @@ const Header = () => {
         <div className="flex items-center justify-between">
           {/* Logo */}
           <div>
-              <img src={logo} alt="logo" className="max-h-10 mt-2"/>
+              <img src={logo} alt="logo" className="max-h-10"/>
           </div>  
 
           {/*menu*/}
           <div className="navigation">
-            <ul className="menu flex items-center gap-10">
+            <ul className="menu flex items-center gap-10 font-bold">
               {navLinks.map((link,index) =>
                <li key={index}>
                     <NavLink to={link.path} className={navClass=> navClass.isActive ? 
-                      'text-primaryColor test-[16px] leading-7 font[600] mt-2' : 
+                      'text-primaryColor test-[16px] leading-7 font[600]' : 
                       'text-textColor test-[16px] leading-7 font[500] hover:text-primaryColor mt-2'}>
                       {link.display}
                     </NavLink>
@@ -54,19 +54,19 @@ const Header = () => {
               <div>
                 <Link to="/">
                 <figure className="w-[35px] h-[35px] rounded-full cursor-pointer">
-                  <img src={userImg} className='w-full rounded-full mt-2' alt="avatarIcon" />
+                  <img src={userImg} className='w-full rounded-full' alt="avatarIcon" />
                 </figure>                
                 </Link>
               </div>
 
               <Link to="/login">
-                <button className="bg-primaryColor text-white px-6 py-2 mt-2 font-[600] h-[44px] flex items-center justify-center rounded-[50px]">
+                <button className="bg-primaryColor text-white px-6 py-2 font-[600] h-[44px] flex items-center justify-center rounded-[50px]">
                   Login
                 </button>
               </Link>
 
               <span className="md:hidden">
-                <BiMenu className= "text-[30px] mt-2 cursor-pointer"/>
+                <BiMenu className= "text-[30px] cursor-pointer"/>
               </span>
 
           </div>
