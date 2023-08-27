13e4f487d609b50618d012664b9f25ceca73839d
RavinduKavinda <dlravindukavinda@gmail.com>
2023-08-27 16:07:44 +0530

    map servicescard

--- frontend/src/components/Services/ServiceList.jsx
+++ frontend/src/components/Services/ServiceList.jsx
@@ -4,8 +4,8 @@ import ServicesCard from './ServicesCard';
 
 const ServiceList = () => {
   return (
-    <div>
-      ServiceList
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-8 mt-8 lg:mt-14">
+      {services.map((item, index) => <ServicesCard item={item} index={index} key={index}/>)}  
     </div>
   );
 }
