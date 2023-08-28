10a3239bdeb1008896903f751baf7fc7c6869899
RavinduKavinda <dlravindukavinda@gmail.com>
2023-08-28 17:57:47 +0530

    css

--- frontend/src/components/Doctors/DoctorList.jsx
+++ frontend/src/components/Doctors/DoctorList.jsx
@@ -4,7 +4,7 @@ import DoctorCard from './DoctorCard';
 
 const DoctorList = () => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3 lg:gap-[30px] mt-[30px] lg:mt[50px]">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-1 lg:gap-[20px] mt-[10px] lg:mt[20px]">
       {doctors.map((doctor)=> 
         <DoctorCard key={doctor.id} doctor={doctor}/>      
       )}
