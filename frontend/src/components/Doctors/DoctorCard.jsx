33d585e0c5834076afcb51734e4b8c27e7319660
RavinduKavinda <dlravindukavinda@gmail.com>
2023-08-28 17:58:09 +0530

    img added

--- frontend/src/components/Doctors/DoctorCard.jsx
+++ frontend/src/components/Doctors/DoctorCard.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 
-const DoctorCard = () => {
+
+const DoctorCard = ({doctor}) => {
+
+    const {
+        name, 
+        specialty, 
+        avgRating, 
+        totalRating, 
+        photo, 
+        totalPatients, 
+        hospital } = doctor
   return (
-    <div>
-      DoctorCard
+    <div className="p-3 lg:p-10">
+        <div>
+            <img src={photo} alt="" className="w-[350px] rounded-[5%] justify-center"/>
+        </div>
+      
     </div>
   );
 }
