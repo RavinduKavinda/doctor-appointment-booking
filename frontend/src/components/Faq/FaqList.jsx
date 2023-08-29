218e6c70bb20f22b685b7b5adead846f6f5d3e90
RavinduKavinda <dlravindukavinda@gmail.com>
2023-08-29 18:28:57 +0530

    map faq

--- frontend/src/components/Faq/FaqList.jsx
+++ frontend/src/components/Faq/FaqList.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import {faqs} from './../../assets/data/faqs';
+import FaqCard from './FaqCard';
 
 const FaqList = () => {
   return (
-    <div>
-      FaqList
-    </div>
+    <ul className="mt-[20px]">
+        {faqs.map((item, index) => 
+            <FaqCard item={item} key={index}/>
+        )}
+    </ul>
   );
-}
+ }
 
 export default FaqList;
