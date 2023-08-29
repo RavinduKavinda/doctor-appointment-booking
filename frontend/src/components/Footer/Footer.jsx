0e89d6ae29793ed6db7509ff276f134b12d31fbe
RavinduKavinda <dlravindukavinda@gmail.com>
2023-08-29 23:45:53 +0530

    socialMedia links

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,4 +1,27 @@
-import React from 'react'
+import React from 'react';
+import {Link} from 'react-router-dom';
+import logo from '../../assets/images/logo.png';
+import {AiFillFacebook, AiFillInstagram, AiFillTwitterCircle, AiFillYoutube} from 'react-icons/ai';
+
+const socialMedia = [
+  {
+    path: "https://www.facebook.com/",
+    icon: <AiFillFacebook className="group-hover:text-white w-4 h-5"/>,
+  },
+  {
+    path: "https://www.instagram.com/",
+    icon: <AiFillInstagram className="group-hover:text-white w-4 h-5"/>,
+  },
+  {
+    path: "https://www.twitter.com/",
+    icon: <AiFillTwitterCircle className="group-hover:text-white w-4 h-5"/>,
+  },
+  {
+    path: "https://www.youtube.com/",
+    icon: <AiFillYoutube className="group-hover:text-white w-4 h-5"/>,
+  },
+];
+
 
 const Footer = () => {
   return (
