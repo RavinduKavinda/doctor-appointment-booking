dceff5df181342d84c126b0893200c1da5ca97b2
RavinduKavinda <dlravindukavinda@gmail.com>
2023-08-29 19:40:21 +0530

    toggle button

--- frontend/src/components/Faq/FaqCard.jsx
+++ frontend/src/components/Faq/FaqCard.jsx
@@ -1,15 +1,30 @@
 import { useState } from 'react';
+import { AiOutlinePlus, AiOutlineMinus } from "react-icons/ai";
 
 const FaqCard = ({ item }) => {
-  return (
-    <div className="p-2 lg:p-4 rounded-[12px] border-[3px] border-solid border-blue-400 ml-[20px] lg:ml-[40px] mr-[20px] lg:mr-[40px] mb-3 cursor-pointer">
-      <div className="flex items-center justify-between gap-6">
-        <h4 className="text-[16px] lg:text-[22px] leading-7 lg:leading-8 text-headingColor">
-            {item.question}
-        </h4>
-      </div>
-    </div>
-  );
-}
+        const [isOpen, setIsOpen] = useState(false);
+
+        const toggle = () => {
+            setIsOpen(!isOpen);
+        };
+
+    return (
+        <div className="p-2 lg:p-4 rounded-[12px] border-[3px] border-solid border-blue-400 ml-[20px] lg:ml-[40px] mr-[20px] lg:mr-[40px] mb-3 cursor-pointer">
+            <div className="flex items-center justify-between gap-6"
+                onClick={toggle}>
+
+                <h4 className="text-[16px] leading-7 lg:text-[22px] lg:leading-8 text-headingColor">
+                    {item.question}
+                </h4>
+            <div
+                className={`${
+                    isOpen && "bg-primaryColor text-white border-none"}
+                    w-6 h-6 lg:w-8 lg:h-8 border border-solid border-blue-400 rounded flex items-center justify-center`}>
+                    {isOpen? <AiOutlineMinus />: <AiOutlinePlus />}
+             </div>
+            </div>
+        </div>
+      );
+    }
 
 export default FaqCard;
