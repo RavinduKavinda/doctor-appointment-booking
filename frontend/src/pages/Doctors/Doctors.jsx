2a62d6361fbc046516ee9c4f0fcbdea1b6e88d75
Ravindu Kavinda <dlravindukavinda@gmail.com>
2023-09-10 00:47:16 +0530

    search bar addded

--- frontend/src/pages/Doctors/Doctors.jsx
+++ frontend/src/pages/Doctors/Doctors.jsx
@@ -5,13 +5,21 @@ import {doctors} from './../../assets/data/doctors'
 const Doctors = () => {
   return (
 <>
+
+{/*-----Search Bar-----*/}
 <section className="bg-[#f5f6fa] " >
     <div className="container text-center">
       <h2 className='heading'>Find a Doctor</h2>
       <div className="max-w-[570px] mt-[30px] mx-auto bg-[#596275] rounded-md flex items-center justify-between">
+        
         <input type="search" 
-               className="py-4 pl-4 pr-2 bg-transparent w-full focus:outline-none cursor-pointer text-white font-bold placeholder-white"
+               className="py-2 pl-4 pr-10 bg-transparent w-full focus:outline-none cursor-pointer text-white font-bold placeholder-white"
                placeholder= "Search Doctor"/>
+        
+        <button className="btn mt-0 rounded-[0px] rounded-r-md">
+          Search
+        </button>
+
       </div>
     </div>
 </section>
