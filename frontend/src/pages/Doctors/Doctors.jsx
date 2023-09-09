6fb99751fe2d73bea85889ba55db9c6af8d9bdf7
Ravindu Kavinda <dlravindukavinda@gmail.com>
2023-09-10 00:40:28 +0530

    search bar

--- frontend/src/pages/Doctors/Doctors.jsx
+++ frontend/src/pages/Doctors/Doctors.jsx
@@ -5,9 +5,15 @@ import {doctors} from './../../assets/data/doctors'
 const Doctors = () => {
   return (
 <>
-<section>
-
-
+<section className="bg-[#f5f6fa] " >
+    <div className="container text-center">
+      <h2 className='heading'>Find a Doctor</h2>
+      <div className="max-w-[570px] mt-[30px] mx-auto bg-[#596275] rounded-md flex items-center justify-between">
+        <input type="search" 
+               className="py-4 pl-4 pr-2 bg-transparent w-full focus:outline-none cursor-pointer text-white font-bold placeholder-white"
+               placeholder= "Search Doctor"/>
+      </div>
+    </div>
 </section>
 </>)
 }
