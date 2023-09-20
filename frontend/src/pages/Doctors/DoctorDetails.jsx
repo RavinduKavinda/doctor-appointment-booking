267543bb2fc5c90b3e30664b1d93c00b32672a5a
Ravindu Kavinda <dlravindukavinda@gmail.com>
2023-09-20 17:24:46 +0530

    styles changed

--- frontend/src/pages/Doctors/DoctorDetails.jsx
+++ frontend/src/pages/Doctors/DoctorDetails.jsx
@@ -12,8 +12,8 @@ const DoctorDetails = () => {
           {/*-----Desc-----*/}
           <div className="md:col-span-2">
             <div className="flex items-center gap-5">
-              <figure className="max-w-[200px] max-h-[400px]">
-                <img src={doctorImg} alt="" className="w-full" />
+              <figure className="max-w-[300px] max-h-[500px]">
+                <img src={doctorImg} alt="" className="w-full rounded-[10px]" />
               </figure>
 
               <div>
@@ -23,9 +23,9 @@ const DoctorDetails = () => {
                 <h3 className="text-headingColor text-[22px] leading-9 mt-3 font-bold">Dr. Emily Rodriguez</h3>
                 <div className="flex items-center gap-[6px]">
                   <span className="flex items-center gap-[6px] text-[14px] leading-5 lg:text-[16px] lg:leading-7 font-semibold text-headingColor">
-                  <img src={starIcon} alt="" className="w-[20px]"/> 4.8 
+                  <img src={starIcon} alt="" className="w-[15px]"/> 4.8 
                   </span> 
-                  <span className="text-[14px] leading-5 lg:text-[16px] lg:leading-7 font-[400] text-textColor">
+                  <span className="flex text-[14px] leading-5 lg:text-[16px] lg:leading-7 font-[400] text-textColor">
                     (300)
                   </span>
                 </div>
