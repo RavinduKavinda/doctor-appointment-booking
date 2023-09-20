df9c38c03cfa56d6c07c8c9de59c379a1179e773
Ravindu Kavinda <dlravindukavinda@gmail.com>
2023-09-21 00:32:50 +0530

    Doctor about

--- frontend/src/pages/Doctors/DoctorAbout.jsx
+++ frontend/src/pages/Doctors/DoctorAbout.jsx
@@ -3,7 +3,13 @@ import React from 'react';
 const DoctorAbout = () => {
   return (
     <div>
-      DoctorAbout
+      <div>
+        <div>
+            <h3 className="text-[20px] leading-7 text-headingColor font-bold flex items-center gap-2">
+                About Of
+            </h3>
+        </div>
+    </div>
     </div>
   );
 }
