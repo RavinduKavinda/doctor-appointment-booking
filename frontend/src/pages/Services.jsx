687bafadf4dd1cbf2c9385298251ad06d28d0fbd
Ravindu Kavinda <dlravindukavinda@gmail.com>
2023-09-20 17:04:55 +0530

    added services page

--- frontend/src/pages/Services.jsx
+++ frontend/src/pages/Services.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { services } from '../assets/data/services'
+import ServicesCard from '../components/Services/ServicesCard'
 
 const Services = () => {
   return (
-    <div>Services</div>
+    <section>
+      <div className="container">
+        <h2 className="heading text-center">
+          Our Services
+        </h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 lg:gap-10 ">
+        {services.map((item, index) => <ServicesCard item={item} index={index} key={index}/>)}  
+    </div>
+      </div>
+    </section>
   )
 }
 
