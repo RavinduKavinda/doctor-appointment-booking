2c98b62857a8d934c5ff40fbb5145cd0dd91c6ea
Ravindu Kavinda <dlravindukavinda@gmail.com>
2023-09-21 16:53:59 +0530

    raating textarea

--- frontend/src/pages/Doctors/FeedbackForm.jsx
+++ frontend/src/pages/Doctors/FeedbackForm.jsx
@@ -5,9 +5,26 @@ const FeedbackForm = () => {
 
     const [rating, setRating] = useState(0)
     const [hover, setHover] = useState(0)
+    const [text, setText] = useState("")
+
+    const handleSubmitReview = async e => {
+        e.preventDefault();
+        // api
+    };
 
   return (
     <form>
+        <div className="mt-8">
+            <h3 className="text-headingColor text-[16px] leading-6 font-bold mb-3">
+                Share your experience
+            </h3>
+
+            <textarea rows="7" placeholder='Write your massage' 
+                className='border border-solid border-textColor focus:outline outline-primaryColor px-4 py-3 rounded-xl w-full'
+                onChange={ e => setText(e.target.value)}>
+            </textarea>
+        </div>
+
         <div>
             <h3 className="text-headingColor text-[16px] leading-6 font-bold mb-3">
                 Rate the overall experience?
@@ -37,6 +54,10 @@ const FeedbackForm = () => {
                 })}
             </div>
         </div>
+
+        <button type='submit' onClick={handleSubmitReview} className='btn'>
+            Submit Feedback
+        </button>
     </form>
   )
 }
