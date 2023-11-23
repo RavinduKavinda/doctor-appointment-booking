e67dd4d942ba9b61c75b852a9c4e225ce2d92ed6
Ravindu Kavinda <dlravindukavinda@gmail.com>
2023-11-23 11:25:28 +0530

    user register

--- backend/Controllers/authControl.js
+++ backend/Controllers/authControl.js
@@ -1,8 +1,61 @@
+import User from "../models/User";
+import Doctor from "../models/Doctor"
+import jwt from "jsonwebtoken"
+import bcrypt from "bcryptjs"
+
 export const register = async (req,res) => {
+
+  const {name, email, password, role, gender, photo} = req.body
+
   try{
+
+    let user = null
+
+    if( role === 'patient') {
+      user = User.findOne({email})
+    }
+    else if ( role === 'doctor' ) {
+      user = Doctor.findOne({email})
+    }
+
+    //user already exist
+    if(user) {
+      return res.status(400).json({message:"User already exist!"})
+    }
+
+    //hash password
+    const salt = await bcrypt.genSalt(10)
+    const hashPassword = await bcrypt.hash(password, salt)
+
+    if( role === 'patient' ) {
+      user = new User ({
+        name,
+        email,
+        password: hashPassword,
+        role,
+        gender,
+        photo
+      })
+    }
+
+    if( role === 'doctor' ) {
+      user = new Doctor ({
+        name,
+        email,
+        password: hashPassword,
+        role,
+        gender,
+        photo
+      })
+    }
+
+    await user.save()
+
+    res.status(200).json({success:true, message: 'User successfully created'})
+
     
   }catch(err){
-    
+    res.status(500).json({success:false, message: 'Internal server error, Try again'})
   }
 };
 
