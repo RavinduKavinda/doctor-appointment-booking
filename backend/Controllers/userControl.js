0d1cca64fc7542ab8b9c7e9c07c4a8029dccfe5d
Ravindu Kavinda <dlravindukavinda@gmail.com>
2023-12-22 19:10:02 +0530

    user control - Delete, single and all users

--- backend/Controllers/userControl.js
+++ backend/Controllers/userControl.js
@@ -5,7 +5,7 @@ export const updateUser = async (req, res) => {
     const id = req.params.id;
 
     try {
-        const updatedUser = await UserSchema.findByIdAndUpdate(
+        const updatedUser = await User.findByIdAndUpdate(
             id,
             { $set: req.body },
             { new: true}
@@ -25,7 +25,83 @@ export const updateUser = async (req, res) => {
             .json({
                 success: false,
                 message: "Failed to Update!",
-                data: updatedUser,
+            });
+    }
+}
+
+//User Delete
+export const deleteUser = async (req, res) => {
+    const id = req.params.id;
+
+    try {
+        await User.findByIdAndDelete(
+            id,
+        );
+
+        res
+            .status(200)
+            .json({
+                success: true,
+                message: "Successfully Deleted!",
+            });
+
+    } catch (error) {
+         res
+            .status(500)
+            .json({
+                success: false,
+                message: "Failed to Delete!",
+            });
+    }
+}
+
+//Single User
+export const getSingleUser = async (req, res) => {
+    const id = req.params.id;
+
+    try {
+        const user = await User.findById(
+            id,
+        );
+
+        res
+            .status(200)
+            .json({
+                success: true,
+                message: "Successfully found!",
+                data: user,
+            });
+
+    } catch (error) {
+         res
+            .status(404)
+            .json({
+                success: false,
+                message: "Failed to found user!",
+            });
+    }
+}
+
+//All Users
+export const getAllUser = async (req, res) => {
+
+    try {
+        const users = await User.find({});
+
+        res
+            .status(200)
+            .json({
+                success: true,
+                message: "Users found!",
+                data: users,
+            });
+
+    } catch (error) {
+         res
+            .status(404)
+            .json({
+                success: false,
+                message: "Failed to found users!",
             });
     }
 }
