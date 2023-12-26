17d4c63af0d825ca1db8c8129787035eded9eed8
Ravindu Kavinda <dlravindukavinda@gmail.com>
2023-12-26 17:59:34 +0530

    protect the users

--- backend/auth/verifyToken.js
+++ backend/auth/verifyToken.js
@@ -1,4 +1,4 @@
-import Jwt from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 import Doctor from "../models/DoctorSchema.js";
 import User from "../models/UserSchema.js";
 
@@ -44,4 +44,31 @@ export const authenticate = async (req, res, next) => {
                 message: "Invalid Token"
             });
     }
+}
+
+//protect the route
+export const restrict = roles => async(req, res, next) => {
+    const userId = req.userID;
+
+    let user;
+
+    const patient = await User.findById(userId);
+    const doctor = await Doctor.findById(userId);
+
+    if(patient){
+        user = patient;
+    }
+    if(doctor){
+        user = doctor;
+    }
+
+    if(!roles.includes(user.role)) {
+        return res
+            .status(401)
+            .json({
+                success:false,
+                message: "You are not Authorized!"
+            });
+    }
+    next();
 }
