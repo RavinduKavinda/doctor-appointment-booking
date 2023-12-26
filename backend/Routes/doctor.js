1e535054217bc5218115e9fd4544ceee00db4329
Ravindu Kavinda <dlravindukavinda@gmail.com>
2023-12-26 18:04:39 +0530

    auth restrict - doctors

--- backend/Routes/doctor.js
+++ backend/Routes/doctor.js
@@ -5,12 +5,14 @@ import {
     getSingleDoctor,
     getAllDoctor
 } from "../Controllers/doctorControl.js";
+import { authenticate, restrict } from "../auth/verifyToken.js";
+
 
 const router = express.Router();
 
-router.get("/:id",getSingleDoctor);
-router.get("/",getAllDoctor);
-router.put("/:id",updateDoctor);
-router.delete("/:id",deleteDoctor);
+router.get("/:id", getSingleDoctor);
+router.get("/", getAllDoctor);
+router.put("/:id",authenticate, restrict(["doctor"]), updateDoctor);
+router.delete("/:id",authenticate, restrict(["doctor"]), deleteDoctor);
 
 export default router;
