2040d46d4a77d1136a83fca86a06f515a7d40ea5
ravidukavinda <dlravindukavinda@gmail.com>
2024-01-07 00:10:56 +0530

    login to web

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,8 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState, useContext } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { BASE_URL } from '../config';
+import { toast } from 'react-toastify';
+import {authContext} from '../context/AuthContext.jsx'
 
 const Login = () => {
 
@@ -8,9 +11,55 @@ const Login = () => {
     password:'',
   });
 
+  const [loading, setLoading] = useState(false)
+  const navigate = useNavigate()
+  const {dispatch} = useContext(authContext)
+
   const handleInputChange = e => {
     setformData({ ... formData, [e.target.name]: e.target.value});
   };
+
+  const submitHandler = async event => {
+
+    event.preventDefault();
+    setLoading(true)
+
+    try {
+      const res = await fetch (`${BASE_URL}/auth/login`,
+      {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+      })
+
+      const result = await res.json()
+
+      if(!res.ok){
+        throw new Error(result.message)
+      }
+
+      dispatch({
+        type: "LOGIN_SUCCESS",
+        payload: {
+          user: result.data,
+          token: result.token,
+          role: result.role,
+        },
+      });
+
+      console.log(result, "login data");
+
+      setLoading(false)
+      toast.success(result.message);
+      navigate('/home')
+
+    } catch (err) {
+      toast.error(err.message)
+      setLoading(false)
+    }
+  }
 
   return (
     <section className="px-5 lg:px-3">
@@ -19,7 +68,7 @@ const Login = () => {
           Hello! Welcome Back <span className="text-primaryColor">Dr.OnCall</span>👨‍⚕️
         </h3>
 
-        <form className="py-4 md:py-1">
+        <form className="py-4 md:py-1" onSubmit={submitHandler}>
           <div className="mb-5">
             <input 
               type="email" 
